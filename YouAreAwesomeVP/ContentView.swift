ec9e1fba15603cddd4fc498ac58f5a54fc20d09a
Bob Witmer <bwitmer@golden.net>
2025-07-15 16:05:43 -0400

    Chapter 1.2 completed

--- YouAreAwesomeVP/ContentView.swift
+++ YouAreAwesomeVP/ContentView.swift
@@ -10,20 +10,24 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
+    @State private var message = "I am a Programmer!"
     var body: some View {
         
         VStack {
-//            Model3D(named: "Scene", bundle: realityKitContentBundle)
-//                .padding(.bottom, 50)
+            //            Model3D(named: "Scene", bundle: realityKitContentBundle)
+            //                .padding(.bottom, 50)
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-
-            Text("I am an app developer!")
+            
+            Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+            Button("Click Me!") {
+                message = "Awesome!"
+            }
         }
         .padding()
         
